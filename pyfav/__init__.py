8c672518ab67a01f129b882405a2766dd9e2b1e5
lhl <lhl@randomfoo.net>
2015-09-18 01:52:28 -0700

    fixed for python3

--- pyfav/__init__.py
+++ pyfav/__init__.py
@@ -1,3 +1,8 @@
-from pyfav import get_favicon_url
-from pyfav import parse_markup_for_favicon
-from pyfav import download_favicon
+try:
+  from pyfav import get_favicon_url
+  from pyfav import parse_markup_for_favicon
+  from pyfav import download_favicon
+except:
+  from pyfav.pyfav import get_favicon_url
+  from pyfav.pyfav import parse_markup_for_favicon
+  from pyfav.pyfav import download_favicon
